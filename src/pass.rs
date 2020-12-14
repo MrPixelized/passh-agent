04b225fd01a6245bafe439cd21bd586d7734c547
Ischa Abraham <contact@ischa.dev>
2020-12-14 21:50:01 +0100

    implemented function to call pass

--- src/pass.rs
+++ src/pass.rs
@@ -1,3 +1,15 @@
+use std::process::Command;
+
 /// Get a datum from pass using the `pass show' command.
-pub fn query(key: String) {
+pub fn query(key: String) -> Result<String, String> {
+    let output = Command::new("pass")
+        .arg("show")
+        .arg(key)
+        .output()
+        .expect("Failed to call Pass binary");
+
+    let res = String::from_utf8(output.stdout)
+        .expect("Pass output has invalid format");
+
+    Ok(res)
 }
