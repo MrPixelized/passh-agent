290fe316a8458fe302084944ddcc1b1b03903280
Ischa Abraham <contact@ischa.dev>
2020-12-29 04:33:34 +0100

    cleaned up error handling for main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ use config::Config;
 use std::collections::HashMap;
 use std::env::temp_dir;
 use std::fs::remove_file;
+use std::error;
+use std::fmt;
 
 use ssh_agent::agent::Agent;
 use ssh_agent::proto::Blob;
@@ -21,6 +23,72 @@ use ssh_agent::proto::Signature;
 
 use openssl::sign::Signer;
 use openssl::hash::MessageDigest;
+
+#[derive(Debug)]
+enum Error {
+    ProtoError(ssh_agent::proto::ProtoError),
+    PassError(pass::Error),
+    KeyError(key_handling::Error),
+    OpensslError(openssl::error::ErrorStack),
+    MissingPubkeyError,
+    UnimplementedError,
+}
+
+impl From<ssh_agent::proto::ProtoError> for Error {
+    fn from(err: ssh_agent::proto::ProtoError) -> Self {
+        Error::ProtoError(err)
+    }
+}
+
+impl From<pass::Error> for Error {
+    fn from(err: pass::Error) -> Self {
+        Error::PassError(err)
+    }
+}
+
+impl From<key_handling::Error> for Error {
+    fn from(err: key_handling::Error) -> Self {
+        Error::KeyError(err)
+    }
+}
+
+impl From<openssl::error::ErrorStack> for Error {
+    fn from(err: openssl::error::ErrorStack) -> Self {
+        Error::OpensslError(err)
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::ProtoError(_) =>
+                write!(f, "Failed to read public SSH key format."),
+            Self::PassError(_) =>
+                write!(f, "The pass binary failed."),
+            Self::KeyError(_) =>
+                write!(f, "Something went wrong parsing or using the keys."),
+            Self::OpensslError(_) =>
+                write!(f, "OpenSsl failed to sign the payload."),
+            Self::MissingPubkeyError =>
+                write!(f, "Public key is not in cache."),
+            Self::UnimplementedError =>
+                write!(f, "The SSH-Agent protocol is not fully implemented.")
+        }
+    }
+}
+
+impl error::Error for Error {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        match self {
+            Self::ProtoError(err) => Some(err),
+            Self::PassError(err) => Some(err),
+            Self::KeyError(err) => Some(err),
+            Self::OpensslError(err) => Some(err),
+            Self::MissingPubkeyError => None,
+            Self::UnimplementedError => None,
+        }
+    }
+}
 
 struct PassSshAgent {
     /// The query string passed to `pass show'.
@@ -69,32 +137,32 @@ impl PassSshAgent {
 
     /// Sign the given data using the private key corresponding to the given
     /// pubkey blob
-    fn sign(&self, pubkey_blob: &Vec<u8>, data: &Vec<u8>) -> Result<SignatureBlob, ()> {
+    fn sign(&self, pubkey_blob: &Vec<u8>, data: &Vec<u8>) -> Result<SignatureBlob, Error> {
         // Check if the given public key is in the cache
         let pubkey = &PublicKey::from_blob(pubkey_blob)?;
 
         if !self.key_map.contains_key(pubkey) {
-            return Err(());
+            return Err(Error::MissingPubkeyError);
         }
 
         // Get the privkey from pass using the cached query
         let privkey_query = &self.key_map[pubkey];
-        let privkey_raw = pass::query(privkey_query.to_owned()).unwrap();
-        let privkey = privkey_raw.to_private_key().unwrap();
+        let privkey_raw = pass::query(privkey_query.to_owned())?;
+        let privkey = privkey_raw.to_private_key()?;
 
         // Sign the message using sha-512 (supported by default on newer openssh instances)
-        let mut signer = Signer::new(MessageDigest::sha512(), &privkey).unwrap();
-        signer.update(data).unwrap();
+        let mut signer = Signer::new(MessageDigest::sha512(), &privkey)?;
+        signer.update(data)?;
         let signature = Signature {
             algorithm: String::from("rsa-sha2-512"),
-            blob: signer.sign_to_vec().unwrap(),
+            blob: signer.sign_to_vec()?,
         };
 
-        Ok(signature.to_blob().unwrap())
+        Ok(signature.to_blob()?)
     }
 
     /// Generate a response to a message from a client.
-    fn handle(&self, message: Message) -> Result<Message, ()> {
+    fn handle(&self, message: Message) -> Result<Message, Error> {
         match message {
             Message::RequestIdentities =>
                 Ok(Message::IdentitiesAnswer(self.get_identities())),
@@ -104,7 +172,7 @@ impl PassSshAgent {
                 ..
             }) =>
                 Ok(Message::SignResponse(self.sign(&pubkey_blob, &data)?)),
-            _ => Err(()),
+            _ => Err(Error::UnimplementedError),
         }
     }
 }
